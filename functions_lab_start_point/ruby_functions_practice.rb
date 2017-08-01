1ee2ead24fac8755b8f886dbd2e50bf1bdd7f654
David Mulligan <fmu2174304@hotmail.com>
2017-08-01 16:44:14 +0100

    Add progress after lab

---
+++ functions_lab_start_point/ruby_functions_practice.rb
@@ -0,0 +1,34 @@
+def add(number_1, number_2)
+  addition = number_1 + number_2
+  return addition
+end
+
+def subtract(number_1, number_2)
+  minus = number_1 - number_2
+  return minus
+end
+
+def multiply(number_1, number_2)
+  multi = number_1 * number_2
+  return multi
+end
+
+def divide(number_1, number_2)
+  div = number_1 / number_2
+  return div
+end
+
+def length_of_string(string)
+  length = string.length
+  return length
+end
+
+def join_string(string_1, string_2)
+  song = string_1 + string_2
+  return song
+end
+
+def add_string_as_number(string_1, string_2)
+  add = string_1.to_i + string_2.to_i
+  return add
+end
